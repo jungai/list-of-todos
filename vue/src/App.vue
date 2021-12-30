83230db4af3779ff906b55a339ac390ad5dabaed
jungai <juworaphol125@gmail.com>
2021-12-30 17:50:33 +0700

    add vue

---
+++ vue/src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+// reactive state
+const newTodo = ref("");
+const todos = ref<string[]>([]);
+
+const addTodo = () => {
+  todos.value.push(newTodo.value);
+  newTodo.value = "";
+};
+
+const removeTodo = (index: number) => {
+  todos.value.splice(index, 1);
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1>With <span class="vue">Vue</span></h1>
+    <input
+      class="input"
+      v-model="newTodo"
+      placeholder="กรอก ...."
+      @keyup.enter="addTodo"
+    />
+    <ul class="display">
+      <li class="item" v-for="(todo, index) in todos" :key="index">
+        {{ todo }}
+        <button @click="removeTodo(index)">X</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+@import "../../core.css";
+</style>
